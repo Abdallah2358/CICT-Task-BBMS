b6cbace0f953055528594b34a71d5ebccc4b7ae0
Abdallah El Hadidi <51133202+Abdallah2358@users.noreply.github.com>
2023-10-30 18:11:22 +0200

    added validation to donor registration

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const Auth = require('../controllers/AuthController');
-
+const {  checkSchema} = require('express-validator');
+const {registerSchema} = require('../validators/auth/registerSchema');
 /* Get Register page. */
-router.get('/register', Auth.GetRegister);
+router.get('/register', Auth.GetDonorsRegister);
 
 /* Post Register Data */
 
-router.post('/register', Auth.PostRegister);
+router.post('/register', checkSchema(registerSchema) ,Auth.PostDonorsRegister);
 module.exports = router;
