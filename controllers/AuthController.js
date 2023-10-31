16721cae93e174861b27596be7f36c15c9466803
Abdallah El Hadidi <51133202+Abdallah2358@users.noreply.github.com>
2023-10-31 11:08:21 +0200

    refactored using env vars

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,17 +1,9 @@
-//js
-const nodemailer = require("nodemailer");
 const db = require('../models');
 const { validationResult } = require('express-validator');
 const Donor = db.Donor;
 const DonationRequest = db.DonationRequest;
-const transport = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-        user: "7adb1ed27ddce9",
-        pass: "dad80e5997a305"
-    }
-});
+const { transport } = require('../config');
+
 const GetDonorsRegister = (req, res, next) => {
     res.render('donors/register', { title: 'register', layout: './layouts/signin', errors: [], donor: {} });
 }
@@ -19,8 +11,7 @@ const PostDonorsRegister = async (req, res, next) => {
     const result = validationResult(req);
     const donor = Donor.build(req.body);
     if (result.isEmpty()) {
-        await donor.save();
-       const dr=  await DonationRequest.create({
+        const dr = await DonationRequest.create({
             donor_id: donor.NID,
             blood_type: donor.blood_type,
             status: "pending",
@@ -33,6 +24,7 @@ const PostDonorsRegister = async (req, res, next) => {
             html: "<h1>Thank you for registering</h1>"
                 + "<h2>you donation is pending approval</h2>",
         });
+        await donor.save();
         res.redirect("/auth/donor/register");
     }
     const errors = result.array()
