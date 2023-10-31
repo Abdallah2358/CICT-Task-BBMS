779ce4fa76d6fcc0061e982735ef1d49c8054343
Abdallah El Hadidi <51133202+Abdallah2358@users.noreply.github.com>
2023-10-31 11:08:53 +0200

    white space refactor

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const Auth = require('../controllers/AuthController');
-const {  checkSchema} = require('express-validator');
-const {registerDonorSchema} = require('../validators/auth/registerDonorSchema');
+const { checkSchema } = require('express-validator');
+const { registerDonorSchema } = require('../validators/auth/registerDonorSchema');
 /* Get Register page. */
 router.get('/donor/register', Auth.GetDonorsRegister);
 
 /* Post Register Data */
 
-router.post('/donor/register', checkSchema(registerDonorSchema) ,Auth.PostDonorsRegister);
+router.post('/donor/register',
+    checkSchema(registerDonorSchema),
+    Auth.PostDonorsRegister);
 module.exports = router;
