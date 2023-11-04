bef38f198de664ef7b6d9b028bb078dd779d4521
Abdallah El Hadidi <51133202+Abdallah2358@users.noreply.github.com>
2023-11-04 02:14:18 +0200

    donation request cycle refactored

--- controllers/DonationRequestController.js
+++ controllers/DonationRequestController.js
@@ -8,7 +8,7 @@ const Donation = db.Donation;
 const DonationRequest = db.DonationRequest;
 
 const index = async (req, res, next) => {
-    const donation_requests = await DonationRequest.findAll({include: 'donor'});
+    const donation_requests = await DonationRequest.findAll({ include: 'donor' });
     res.render('donation-request/index', { title: 'Donation Requests', donation_requests: donation_requests });
 }
 const show = async (req, res, next) => {
@@ -22,33 +22,33 @@ const show = async (req, res, next) => {
 }
 
 const create = async (req, res, next) => {
-    // res.send('aaa')
-    return res.render('donation-request/create', { title: 'Create Donation Request', layout: './layouts/signin', errors: [], donor: {} })
+    // return res.send('aaa')
+    return res.render('donation-request/create', { title: 'Create Donation Request', layout: './layouts/sign-in', errors: [], donor: {} })
 }
 const store = async (req, res, next) => {
     const result = validationResult(req);
     if (!result.isEmpty()) {
         return res.render('donation-request/create', {
-            title: 'Create Donation Request', layout: './layouts/signin',
+            title: 'Create Donation Request', layout: './layouts/sign-in',
             errors: result.array(), donor: req.body
         });
-
     }
     const donor = await Donor.findOne({ where: { national_id: req.body.national_id } });
+    // return res.send(donor);
     if (!donor) {
         // res.send('aaa')
-        return res.redirect('/auth/donor/register');
+        return res.redirect('/donor/register');
     }
-    const last_donation_request = await DonationRequest.findOne({ where: { donor_id: donor.national_id }, order: [['createdAt', 'DESC']] });
+    const last_donation_request = await DonationRequest.findOne({ where: { donor_id: donor.id }, order: [['createdAt', 'DESC']] });
     if (last_donation_request && last_donation_request.status == "pending") {
         return res.render('donation-request/create',
             {
-                title: 'Create Donation Request', layout: './layouts/signin',
+                title: 'Create Donation Request', layout: './layouts/sign-in',
                 errors: [{ msg: "You already have a pending donation request" }],
                 donor: donor
             });
     }
-    const last_donation = await Donation.findOne({ where: { donor_id: donor.national_id }, order: [['createdAt', 'DESC']] });
+    const last_donation = await Donation.findOne({ where: { donor_id: donor.id }, order: [['createdAt', 'DESC']] });
     if (can_donate((new Date()).toISOString(), last_donation?.createdAt)) {
         await transport.sendMail({
             from: "no-reply@bbms.eg",
@@ -58,7 +58,7 @@ const store = async (req, res, next) => {
                 + "<h2>you donation is pending approval of virus test</h2>",
         });
         const dr = await DonationRequest.create({
-            donor_id: donor.national_id,
+            donor_id: donor.id,
             blood_type_id: donor.blood_type_id,
             status: "pending",
             test_result: "pending",
@@ -68,7 +68,7 @@ const store = async (req, res, next) => {
     const next_donation_date = new Date(last_donation.createdAt);
     next_donation_date.setMonth(next_donation_date.getMonth() + 3);
     return res.render('donation-request/create', {
-        title: 'Create Donation Request', layout: './layouts/signin',
+        title: 'Create Donation Request', layout: './layouts/sign-in',
         errors: [
             { msg: "You can't donate now.", },
             { msg: "You must wait 3 month from your last Donation" },
@@ -88,11 +88,15 @@ const get_test_result = (req, res) => {
 }
 const post_test_result = async (req, res) => {
     // res.send('aaa')
-    const dr = await DonationRequest.findOne({ where: { id: req.params.id } });
+    const dr = await DonationRequest.findOne({ where: { id: req.params.id }, include: 'donor' });
+    if (!dr) {
+        return res.redirect('/donation-request');
+    }
+    const donor = dr.donor;
     const lastDonation = await Donation.findOne({ where: { donor_id: dr.donor_id }, order: [['createdAt', 'DESC']] });
-    const donor = await Donor.findOne({ where: { national_id: dr.donor_id } });
-    if (req.body.test_result == 'negative' && can_donate(dr.createdAt, lastDonation?.createdAt)) {
-        dr.status = "accepted";
+    // return res.send({ donor, lastDonation });
+    if (req.body.test_result == 'Negative' && can_donate(dr.createdAt, lastDonation?.createdAt)) {
+        dr.status = "Accepted";
         dr.test_result = req.body.test_result;
         await transport.sendMail({
             from: "no-reply@bbms.eg",
@@ -102,7 +106,7 @@ const post_test_result = async (req, res) => {
                 + "<h2>Thanks for helping save lives </h2>",
         });
         const donation = await Donation.create({
-            donor_id: donor.national_id,
+            donor_id: donor.id,
             blood_type_id: donor.blood_type_id,
         });
         dr.donation_id = donation.id;
@@ -110,7 +114,7 @@ const post_test_result = async (req, res) => {
         await dr.save();
         return res.redirect('/donation-request');
     }
-    dr.status = "rejected";
+    dr.status = "Rejected";
     await transport.sendMail({
         from: "no-reply@bbms.eg",
         to: donor.email,
